5584bf4d03fbc7706a967134270c7c291ec62b1e
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-03-21 11:55:27 +0530

    Update DOWN.py

--- DOWN.py
+++ DOWN.py
@@ -1,5 +1,8 @@
 from pytube import YouTube
-YouTube('https://www.youtube.com/watch?v=himEMfYQJ1w').streams.first().download()
+#replace 'YOUTUBE URL' IN LINE 3 WITH YOUR  OWN URL 
+#line 4 downloads the first quality of video
+YouTube('YOUTUBE URL').streams.first().download()
+#LINES 5,6,7 PRINTS ALL DOWNLOADABLE VIDEO FORMATS/QUALITIES
 streams = yt.streams.all()
 for x in streams:
     print(x)
